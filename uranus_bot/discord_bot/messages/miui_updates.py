337d8c7a3158e1588d23307a5a8f5b6e1d450344
yshalsager <ysh-alsager@hotmail.com>
2020-02-12 19:07:06 +0200

    discord: Implement miui updates module
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

---
+++ uranus_bot/discord_bot/messages/miui_updates.py
@@ -0,0 +1,47 @@
+""" MIUI Updates generator """
+from discord import Embed
+
+from uranus_bot.providers.miui_updates_tracker.miui_updates_tracker import get_miui
+from uranus_bot.utils.miui import get_region, get_type, get_branch
+from uranus_bot import XFU_WEBSITE
+
+
+async def miui_message(device, updates, codenames_names):
+    """ Generate discord message of recovery/fastboot command """
+    data = await get_miui(device, updates)
+    rom_type = await get_type(str(data))
+    embed = Embed(title=f"**Latest {codenames_names[device]}** (`{device}`) **MIUI Official {rom_type} ROMs**")
+    for i in data:
+        version = i['version']
+        android = i['android']
+        download = i['download']
+        region = await get_region(download.split('/')[-1], i['codename'], version)
+        embed.add_field(name=f"{region} {version} | {android}",
+                        value=f'[Download]({download})', inline=True)
+    embed.add_field(name='ROMs Archive', value=f'[Here]({XFU_WEBSITE}/archive/miui/{device}/)')
+    return embed
+
+
+async def archive_message(device, codenames_names):
+    """ Generate discord message of archive command """
+    embed = Embed(title=f'**MIUI ROMs archive for {codenames_names[device]}** (`{device}`)')
+    embed.add_field(name='MIUI Archive',
+                    value=f'[Download]({XFU_WEBSITE}/archive/miui/{device}/)')
+    return embed
+
+
+async def latest_miui_message(device, updates, codenames_names):
+    """ Generate discord message of latest command """
+    data = await get_miui(device, updates)
+    description = ""
+    for i in data:
+        version = i['version']
+        download = i['download']
+        filename = download.split('/')[-1]
+        branch = await get_branch(version)
+        region = await get_region(filename, i['codename'], version)
+        description += f"{region} {branch}: `{version}`\n"
+    embed = Embed(title=f"**Latest MIUI Versions for {codenames_names[device]}** (`{device}`)",
+                  description=description)
+    return embed
+
---
+++ uranus_bot/discord_bot/modules/miui_updates.py
@@ -0,0 +1,34 @@
+""" MIUI Updates command handler """
+from uranus_bot.discord_bot.messages.miui_updates import miui_message, archive_message, latest_miui_message
+from uranus_bot.discord_bot.discord_bot import BOT, PROVIDER
+from uranus_bot.utils.error_message import error_message
+
+
+@BOT.command(aliases=['recovery', 'fastboot'])
+async def miui(ctx, device):
+    """Send latest recovery/fastboot ROMs for a device Example: !recovery whyred / !fastboot whyred"""
+    if device not in PROVIDER.miui_codenames:
+        await ctx.send(await error_message(device))
+        return
+    updates = PROVIDER.miui_recovery_updates if ctx.invoked_with == "recovery" \
+        else PROVIDER.miui_fastboot_updates
+    await ctx.send(None, embed=await miui_message(device, updates,
+                                                  PROVIDER.codenames_names))
+
+
+@BOT.command(name='archive')
+async def archive(ctx, device):
+    """Send MIUI archive downloads link for a device Example: !archive whyred"""
+    if device not in PROVIDER.miui_codenames:
+        await ctx.send(await error_message(device))
+        return
+    await ctx.send(None, embed=await archive_message(device, PROVIDER.codenames_names))
+
+
+@BOT.command(name='latest')
+async def latest(ctx, device):
+    """Send latest MIUI versions for a device Example: !latest whyred"""
+    if device not in PROVIDER.miui_codenames:
+        await ctx.send(await error_message(device))
+        return
+    await ctx.send(None, embed=await latest_miui_message(device, PROVIDER.miui_recovery_updates, PROVIDER.codenames_names))
