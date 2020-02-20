d3bed0218846de900cf11c3e8ed44ddb3c53cd6c
yshalsager <ysh-alsager@hotmail.com>
2020-02-20 21:52:40 +0200

    tg: admin: add backup db job
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- uranus_bot/telegram_bot/modules/admin.py
+++ uranus_bot/telegram_bot/modules/admin.py
@@ -1,11 +1,12 @@
 """Xiaomi Geeks Telegram Bot - admin module"""
 from asyncio import sleep
+from datetime import datetime
 
 from telethon import events
 
-from uranus_bot import TG_BOT_ADMINS
+from uranus_bot import TG_BOT_ADMINS, PARENT_DIR, TG_BOT_DB
 from uranus_bot.messages.admin import stats_message
-from uranus_bot.telegram_bot import DATABASE
+from uranus_bot.telegram_bot import DATABASE, TG_LOGGER
 from uranus_bot.telegram_bot.tg_bot import BOT
 
 
@@ -26,3 +27,18 @@ async def broadcast_handler(event):
         await BOT.send_message(chat[0], message)
         await sleep(2)
     raise events.StopPropagation
+
+
+async def backup_database():
+    """ Send database backup to bot owners daily """
+    while True:
+        for admin in TG_BOT_ADMINS:
+            now = str(datetime.today()).split('.')[0]
+            await BOT.send_message(admin, now,
+                                   file=f"{PARENT_DIR}/{TG_BOT_DB}")
+            TG_LOGGER.info(f"Sent database backup ({now}) to {admin}")
+            await sleep(2)
+        await sleep(60 * 60 * 24)
+
+
+BOT.loop.create_task(backup_database())
