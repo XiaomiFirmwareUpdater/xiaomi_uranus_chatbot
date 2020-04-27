65bbd67a8098487a35705feccefc20589ff8c82e
yshalsager <ysh-alsager@hotmail.com>
2020-04-27 14:29:40 +0200

    providers: of: migrate to V2 API
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- uranus_bot/providers/custom_recovery/orangefox/orangefox.py
+++ uranus_bot/providers/custom_recovery/orangefox/orangefox.py
@@ -10,27 +10,27 @@ async def get_orangefox(device):
     """
     fetch latest orangefox links for a device
     """
-    api_url = "https://api.orangefox.tech"
+    api_url = "https://api.orangefox.download/v2"
     host = "https://files.orangefox.tech"
     async with ClientSession() as session:
         devices = None
         try:
-            devices = json.loads(await fetch(session, f'{api_url}/all_codenames/'))
+            devices = json.loads(await fetch(session, f'{api_url}/device'))
         except json.decoder.JSONDecodeError:
             device = None
-        if device not in devices:
+        if device not in [i['codename'] for i in devices]:
             return
         downloads = []
         try:
-            stable = json.loads(await fetch(session, f'{api_url}/last_stable_release/{device}'))
+            stable = json.loads(await fetch(session, f'{api_url}/device/{device}/releases/stable/last'))
             downloads.append({f"{stable['file_name']}": f"{host}/" + "/".join(stable['file_path'].split('/')[5:])})
         except json.decoder.JSONDecodeError:
             pass
         try:
-            beta = json.loads(await fetch(session, f'{api_url}/last_beta_release/{device}'))
+            beta = json.loads(await fetch(session, f'{api_url}/device/{device}/releases/beta/last'))
             downloads.append({f"{beta['file_name']}": f"{host}/" + "/".join(beta['file_path'].split('/')[5:])})
         except json.decoder.JSONDecodeError:
             pass
         if downloads:
-            info = json.loads(await fetch(session, f'{api_url}/details/{device}'))
-            return {'name': info['fullname'], 'maintainer': info['maintainer'], 'downloads': downloads}
+            info = json.loads(await fetch(session, f'{api_url}/device/{device}'))
+            return {'name': info['fullname'], 'maintainer': info['maintainer']['name'], 'downloads': downloads}
