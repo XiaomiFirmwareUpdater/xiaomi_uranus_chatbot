24c1ba0688fea21b6c705c0aa6f0071f3fbcea9a
yshalsager <ysh-alsager@hotmail.com>
2021-04-03 22:40:01 +0200

    providers: utils: Add type hints to fetch method
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- uranus_bot/providers/utils/utils.py
+++ uranus_bot/providers/utils/utils.py
@@ -1,7 +1,9 @@
 """ Providers utilities """
+from aiohttp import ClientResponse
 
 
-async def fetch(session, url):
+async def fetch(session, url) -> str:
     """ Fetch website page """
+    response: ClientResponse
     async with session.get(url) as response:
         return await response.text()
