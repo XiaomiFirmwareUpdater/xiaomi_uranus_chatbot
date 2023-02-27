1c9574e28ae9bd2fbbf02427fb719cce3b44ff2e
yshalsager <ysh-alsager@hotmail.com>
2023-02-27 14:06:56 +0200

    fix(init): append parent module path before importing
    closes: #119
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- uranus_bot/discord_bot/__init__.py
+++ uranus_bot/discord_bot/__init__.py
@@ -1,5 +1,10 @@
 """ Discord bot initialization """
 import logging
+import sys
+
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
 from uranus_bot.database.discord_database import Database, discord_session
 
--- uranus_bot/telegram_bot/__init__.py
+++ uranus_bot/telegram_bot/__init__.py
@@ -1,5 +1,10 @@
 """ Telegram bot initialization """
 import logging
+import sys
+
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
 from uranus_bot.database.database import tg_session, Database
 
